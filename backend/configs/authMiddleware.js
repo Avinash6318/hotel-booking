862110b410698f30cdd0c647cce5590cf3105b0d
Avinash6318 <muppidiavinash6318@gmail.com>
2026-07-17 09:51:33 +0530

    testing the error

--- backend/configs/authMiddleware.js
+++ backend/configs/authMiddleware.js
@@ -1,26 +1,54 @@
+// import User from "../model/User.js";
+
+// //middleware to check if the user is authenticated
+// export const protect = async(req,res,next)=>{
+//    const { userId } = req.auth;
+
+//     if (!userId) {
+//         return res.status(401).json({
+//             success: false,
+//             message: "not authenticated"
+//         });
+//     }
+
+//     const user = await User.findById(userId);
+
+
+//     if (!user) {
+//         return res.status(404).json({
+//             success: false,
+//             message: "User not found in database"
+//         });
+//     }
+
+//     req.user = user;
+//     next();
+// }
+
 import User from "../model/User.js";
 
-//middleware to check if the user is authenticated
-export const protect = async(req,res,next)=>{
-   const { userId } = req.auth;
+export const protect = async (req, res, next) => {
+    console.log("req.auth =", req.auth);
+    console.log("Authorization =", req.headers.authorization);
+
+    const { userId } = req.auth;
 
     if (!userId) {
         return res.status(401).json({
             success: false,
-            message: "not authenticated"
+            message: "Not authenticated"
         });
     }
 
     const user = await User.findById(userId);
 
-
     if (!user) {
         return res.status(404).json({
             success: false,
-            message: "User not found in database"
+            message: "User not found"
         });
     }
 
     req.user = user;
     next();
-}
+};
